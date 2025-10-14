03864740d70cd3456fb2bd5f331a815933047b99
William Deng <weiduo.deng@gmail.com>
2025-10-14 22:35:08 +0100

    231A Team solution

---
+++ 231A_Team.py
@@ -0,0 +1,9 @@
+n = int(input())
+total_num_probs = 0
+for _ in range(n):
+    P, V, T = map(int, input().split())
+    if P + V + T >= 2:
+        total_num_probs += 1
+    else:
+        continue
+    print(total_num_probs)
--- 71A_Way_Too_Long_Words.py
+++ 71A_Way_Too_Long_Words.py
@@ -1,4 +1,7 @@
 n = int(input())
 for _ in range(n):
     word = input()
-    print(word[0] + str(len(word)-2) + word[-1])
+    if len(word) > 10:
+        print(word[0] + str(len(word)-2) + word[-1])
+    else:
+        print(word)
